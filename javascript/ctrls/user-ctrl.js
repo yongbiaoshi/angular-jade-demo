fc8513d8ef1308ecfcf798dcb5b2c442e554acd2
yongbiaoshi <shyb0904@163.com>
2015-11-16 18:23:51 +0800

    form validation

--- javascript/ctrls/user-ctrl.js
+++ javascript/ctrls/user-ctrl.js
@@ -4,10 +4,19 @@
   app.controller('UserController', ['$scope', '$window', '$log', function($scope, win, $log){
     $scope.user = {};
     $scope.users = [];
-    $scope.saveUser = function(){
+    $scope.saveUser = function(form){
       if($scope.user) {
         $scope.users.push($scope.user);
         $scope.user = {};
+      }
+      $scope.resetForm(form);
+    };
+    $scope.resetForm = function(form){
+      for(var key in form){
+        if(key && key.indexOf('$') !== 0){
+          form[key].$dirty = false;
+          form[key].$pristine = true;
+        }
       }
     };
   }]);
@@ -21,6 +30,24 @@
 
       },
       controllerAs: 'userListCtrl'
+    };
+  });
+
+  app.directive('emailOrMobile', function(){
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModel){
+        var MOBILE_REGEXP = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+        var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+        var validator = function(value){
+          var validity = ngModel.$isEmpty(value) || EMAIL_REGEXP.test(value) || MOBILE_REGEXP.test(value);
+          ngModel.$setValidity('emailOrMobile', validity);
+          return validity ? value : undefined;
+        };
+        ngModel.$formatters.push(validator);
+        ngModel.$parsers.push(validator);
+      }
     };
   });
 
